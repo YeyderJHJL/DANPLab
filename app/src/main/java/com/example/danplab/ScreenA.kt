9904c585cf7393829a46149b3772597029bd0465
yeyderjhjl <jturpoan.@unsa.edu.pe>
2026-04-28 13:15:16 -0500

    lab 3: creación de screens y configuración de navigation

---
+++ app/src/main/java/com/example/danplab/ScreenA.kt
@@ -0,0 +1,25 @@
+package com.example.danplab
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenA(){
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Pantalla A")
+        Button(onClick = {
+        }) {
+            Text(text = "Ir a Pantalla B")
+        }
+    }
+}
