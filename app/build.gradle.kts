69ee4ecdb56f601c159f58a2010807bfb03948a3
yeyderjhjl <jturpoan.@unsa.edu.pe>
2026-05-04 04:08:24 -0500

    lab 3: actualización de librerías

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    implementation("androidx.navigation:navigation-compose:2.9.8")
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
