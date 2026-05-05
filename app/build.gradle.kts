b0b325dd58b56c9026d72f467921ae52df22c790
yeyderjhjl <jturpoan.@unsa.edu.pe>
2026-05-05 13:25:19 -0500

    lab 4: integración de componete de tareas y sus librerías

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material.icons)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/danplab/components/TareaItem.kt
+++ app/src/main/java/com/example/danplab/components/TareaItem.kt
@@ -1,2 +1,35 @@
 package com.example.danplab.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.danplab.model.Tarea
+
+@Composable
+fun TareaItem(
+    tarea: Tarea,
+    onToggle: () -> Unit,
+    onDelete: () -> Unit
+) {
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row {
+            Checkbox(
+                checked = tarea.completado,
+                onCheckedChange = { onToggle() }
+            )
+            Text(tarea.descripcion)
+        }
+
+        IconButton(onClick = onDelete) {
+            Icon(Icons.Default.Delete, contentDescription = "Eliminar")
+        }
+    }
+}
